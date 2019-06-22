03bb291336ee7fb14a560313739aa3b3df4d80d0
PMJ <pmj@pmj.hu>
2019-06-22 15:21:06 +0200

    cities JSON -> JS

---
+++ app/lib/cities.js
@@ -0,0 +1,45 @@
+module.exports = {
+    fremont: {
+        linode: 'us-west'
+    },
+    dallas: {
+        linode: 'us-central'
+    },
+    atlanta: {
+        linode: 'us-southeast'
+    },
+    toronto: {
+        linode: 'ca-central',
+        digitalocean: 'TOR1'
+    },
+    newark: {
+        linode: 'us-east'
+    },
+    london: {
+        linode: 'eu-west',
+        digitalocean: 'LON1'
+    },
+    frankfurt: {
+        linode: 'eu-central',
+        digitalocean: 'FRA1'
+    },
+    singapore: {
+        linode: 'ap-south',
+        digitalocean: 'SGP1'
+    },
+    tokyo: {
+        linode: 'ap-northeast'
+    },
+    newyork: {
+        digitalocean: 'NYC1'
+    },
+    amsterdam: {
+        digitalocean: 'AMS3'
+    },
+    sanfrancisco: {
+        digitalocean: 'SFO2'
+    },
+    bangalore: {
+        digitalocean: 'BLR1'
+    },
+}
