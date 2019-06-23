81283aa2d47b56973c249a6267c472de4a6729ae
PMJ <pmj@pmj.hu>
2019-06-23 17:26:15 +0200

    Fix entrypoint

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # GENERATE NEW SSH KEYPAIR
+rm -rf /root/.ssh/id_rsa /root/.ssh/id_rsa.pub
 ssh-keygen -t rsa -b 4096 -C "noreply@example.com" -f /root/.ssh/id_rsa -N ""
 eval $(ssh-agent) && ssh-add /root/.ssh/id_rsa
 
 # LAUNCH INIT SCRIPT
-node /root/index.js
+node /app/index.js
