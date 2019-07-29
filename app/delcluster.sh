790e57277570e2e4be940e2dcce7ec318056691f
PMJ <pmj@pmj.hu>
2019-07-29 17:50:45 +0200

    Also delete kubeconfig file after removing cluster

--- app/delcluster.sh
+++ app/delcluster.sh
@@ -4,3 +4,8 @@ for id in $(linode-cli linodes list --text | grep $1 | awk '{print $1}')
 do
     linode-cli linodes delete $id
 done
+
+for kubecfg in $(find /out -name *.yaml -type f | grep $1)
+do
+    rm $kubecfg
+done
